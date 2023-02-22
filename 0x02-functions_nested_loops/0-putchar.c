611c1941e6518b98e60a8ffc8271d9a82b715749
collinsxavier <nwodo.collins@gmail.com>
2023-02-21 23:44:05 -0800

    Nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
-#include<stdio.h>
-int main (void)
+#include "_putchar.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
 {
-	/*putchar task*/
-	char vars "_putchar"
-		printf("%d\n: char = _putchar");
-	return(0)
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
 }
