8bcadc343368758011c1f563e397b7cd837545e6
collinsxavier <nwodo.collins@gmail.com>
2023-03-12 01:42:39 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,12 +5,13 @@
  *
  * return: integer value
  */
-int _strlen_recursion(char *s)
+	int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
 return (0);
 }
+
 s++;
 return (_strlen_recursion(s) + 1);
 }
