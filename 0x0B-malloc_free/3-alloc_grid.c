c50a303eeab9675439838ce334c131630c14a315
collinsxavier <nwodo.collins@gmail.com>
2023-08-08 22:13:25 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,9 +18,9 @@ int **alloc_grid(int width, int height)
 	mee = malloc(sizeof(int *) * height);
 	if (mee == NULL)
 	return (NULL);
-	{
+
 	for (x = 0; x < height; x++)
-	}
+
 	{	mee[x] = malloc(sizeof(int) * width);
 		if (mee[x] == NULL)
 		{
@@ -35,6 +35,6 @@ int **alloc_grid(int width, int height)
 	{
 		for (y = 0; y < width; y++)
 		mee[x][y] = 0;
-		return (mee);
 	}
+		return (mee);
 }
